fbaf06e5ea833f4c6e87010f7b16c1237c811a4b
Pierre-Emmanuel Levesque <pierre.e.levesque@gmail.com>
2013-07-19 02:00:53 +0800

    fixed minor issues

--- Zoho_Creator_API.php
+++ Zoho_Creator_API.php
@@ -22,7 +22,7 @@
  * @author      Pierre-Emmanuel Lévesque
  * @email       pierre.e.levesque@gmail.com
  * @copyright   Copyright 2011, Pierre-Emmanuel Lévesque
- * @license     MIT License - @see README.md
+ * @license     MIT License - @see LICENSE.md
  */
 class Zoho_Creator_API {
 
